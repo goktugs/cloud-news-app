04cba73f3d24a4584ec1ee22d6f3272b2a7e8c55
goktugs <gokojapan5@gmail.com>
2023-09-18 01:33:36 +0300

    feat: add validate and refresh token logic

--- server/src/routes/root.js
+++ server/src/routes/root.js
@@ -1,8 +1,12 @@
 import express from "express";
 import getRoot from "../controllers/root/getRoot";
 import postRoot from "../controllers/root/postRoot";
-import signupPostRoot from "../controllers/root/signUpPost.controller";
+import signupPostRoot from "../controllers/root/signupPost.controller";
 import loginPostRoot from "../controllers/root/loginPost.controller";
+import currentUserGet from "../controllers/root/currentUserGet.controller.controller";
+import validateTokenHandler from "../middleware/validateTokenHandler";
+import changeUserPreferencesPut from "../controllers/root/changeUserPreferecencesPut.controller";
+import refreshTokenPost from "../controllers/root/refreshTokenPost.controller";
 
 const root = express.Router();
 
@@ -10,5 +14,12 @@ root.get("/", getRoot);
 root.post("/", postRoot);
 root.post("/api/v1/signup", signupPostRoot);
 root.post("/api/v1/login", loginPostRoot);
+root.get("/api/v1/current", validateTokenHandler, currentUserGet);
+root.post("/api/v1/refreshToken", refreshTokenPost);
+root.put(
+  "/api/v1/changeUserPreferences",
+  validateTokenHandler,
+  changeUserPreferencesPut
+);
 
 export default root;
