7e5b82b0e16bed6481b4918607080760b6573e3f
goktugs <gokojapan5@gmail.com>
2023-09-17 21:06:39 +0300

    feat: add singup be

--- server/src/app.js
+++ server/src/app.js
@@ -1,33 +1,37 @@
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import express from 'express';
-import helmet from 'helmet';
-import morgan from 'morgan';
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
 
-import config from './config';
-import errorHandler from './middleware/errorHandler';
-import fourOhFour from './middleware/fourOhFour';
-import root from './routes/root';
+import config from "./config";
+import errorHandler from "./middleware/errorHandler";
+import fourOhFour from "./middleware/fourOhFour";
+import root from "./routes/root";
 
-const app = express()
+import db from "./config/db.config";
+
+const app = express();
 
 // Apply most middleware first
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(cookieParser())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-app.use(cors({
-    origin: config.clientOrigins[config.nodeEnv]
-}))
+app.use(
+  cors({
+    origin: config.clientOrigins[config.nodeEnv],
+  })
+);
 
-app.use(helmet())
-app.use(morgan('tiny'))
+app.use(helmet());
+app.use(morgan("tiny"));
 
 // Apply routes before error handling
-app.use('/', root)
+app.use("/", root);
 
 // Apply error handling last
-app.use(fourOhFour)
-app.use(errorHandler)
+app.use(fourOhFour);
+app.use(errorHandler);
 
-export default app
+export default app;
--- server/src/routes/root.js
+++ server/src/routes/root.js
@@ -1,10 +1,14 @@
-import express from 'express';
-import getRoot from '../controllers/root/getRoot';
-import postRoot from '../controllers/root/postRoot';
+import express from "express";
+import getRoot from "../controllers/root/getRoot";
+import postRoot from "../controllers/root/postRoot";
+import signupPostRoot from "../controllers/root/signUpPost.controller";
+import loginPostRoot from "../controllers/root/loginPost.controller";
 
-const root = express.Router()
+const root = express.Router();
 
-root.get('/', getRoot)
-root.post('/', postRoot)
+root.get("/", getRoot);
+root.post("/", postRoot);
+root.post("/api/v1/signup", signupPostRoot);
+root.post("/api/v1/login", loginPostRoot);
 
-export default root
+export default root;
